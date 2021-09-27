6ef06d876768d1556f2d4b47bcba076e6c57fb8c
Josh Soref <jsoref@users.noreply.github.com>
2021-09-27 00:52:50 -0400

    spelling: domain
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- applications/luci-app-smartdns/htdocs/luci-static/resources/view/smartdns/smartdns.js
+++ applications/luci-app-smartdns/htdocs/luci-static/resources/view/smartdns/smartdns.js
@@ -426,7 +426,7 @@ return L.view.extend({
 		o.rempty = true
 		o.modalonly = true;
 
-		// Doman address;
+		// Domain address;
 		s = m.section(form.TypedSection, "smartdns", _("Advanced Settings"), _("Advanced Settings"));
 		s.anonymous = true;
 
@@ -457,7 +457,7 @@ return L.view.extend({
 			return fs.write('/etc/smartdns/blacklist-ip.conf', formvalue.trim().replace(/\r\n/g, '\n') + '\n');
 		};
 
-		// Doman address;
+		// Domain address;
 		s = m.section(form.TypedSection, "smartdns", _("Technical Support"),
 			_("If you like this software, please buy me a cup of coffee."));
 		s.anonymous = true;
